89f3420adc3ae08fdf4438e8d0ee735fd9f5ecfc
jw7914 <jw7914@nyu.edu>
2026-04-17 12:11:11 -0400

    fixing import issue

--- src/main/java/quizards/web/StudySetController.java
+++ src/main/java/quizards/web/StudySetController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import quizards.persistence.AppUserEntity;
 import quizards.service.AuthService;
